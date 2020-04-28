42f8f4ca25c1ea6a93295df0b8614426ac528d72
Mark Nyberg <bigusdeveloper@gmail.com>
2020-04-28 11:09:31 -0400

    Generating png's with transparency and using just folder name to find files.

--- project_files/proj_class.py
+++ project_files/proj_class.py
@@ -21,6 +21,8 @@ import numpy as np
 from pprint import pprint as pp
 import os
 from datetime import datetime as dt
+from matplotlib import colors
+from matplotlib import pyplot as plt
 
 
 class SentinelPass:
@@ -29,31 +31,32 @@ class SentinelPass:
         """Initializes class with the paths b4 and b8 jp2's and GeoJSON path"""
         self.safe_path = safe_path
         self._find_files(self.safe_path)
-        self.all_mask = gpd.read_file(gjson_path)
+        self._name_to_time(self.b2_fn)
+        self._all_mask = gpd.read_file(gjson_path)
         self._pull_padded_box()
         self._evi_square()
         self._ndvi_square()
 
-    def _find_files(self, safe_path: str):
+    def _find_files(self, safe_folder: str):
         """Accepts the base path for a set of download files and pulls the string for each desired band"""
-        for root, dirs, files in os.walk(safe_path):
+        for root, dirs, files in os.walk(f'../data/{safe_folder}'):
             for file in files:
                 if file.endswith("B02_10m.jp2"):
-                    #print("Band 2 file " + os.path.join(root, file))
+                    # print("Band 2 file " + os.path.join(root, file))
                     self.b2_path = os.path.join(root, file)
+                    self.b2_fn = file
                 elif file.endswith("B04_10m.jp2"):
-                    #print("Band 4 file " + os.path.join(root, file))
+                    # print("Band 4 file " + os.path.join(root, file))
                     self.b4_path = os.path.join(root, file)
                 elif file.endswith("B08_10m.jp2"):
-                    #print("Band 8 file " + os.path.join(root, file))
+                    # print("Band 8 file " + os.path.join(root, file))
                     self.b8_path = os.path.join(root, file)
-
 
     def _pull_padded_box(self):
         """Pulls padded box info from the geojson file and sets attribute to an epsg:32616 geometry"""
-        pad_geom = self.all_mask[self.all_mask.name == 'Padded box']  # access the geometry that's a box padded around the farm
+        pad_geom = self._all_mask[
+            self._all_mask.name == 'Padded box']  # access the geometry that's a box padded around the farm
         self.pad_geom_m = pad_geom.to_crs('epsg:32616')  # convert this geometry to the frame for
-
 
     def _ndvi_square(self):
         """parses the b4 and b8 data down to a simple square around the farm"""
@@ -74,9 +77,9 @@ class SentinelPass:
         self.ndvi = (self.nir - self.red) / (self.nir + self.red)
         b4_out_meta.update({'dtype': rasterio.float64})
 
-        with rasterio.open('ndvi_masked.tiff', 'w', **b4_out_meta) as dest: # refactor to include datetime in name
+        self.ndvi_gtf_name = f"./pics/ndvi_{self.sense_date.strftime('%Y%m%d')}.tiff"
+        with rasterio.open(self.ndvi_gtf_name, 'w', **b4_out_meta) as dest:
             dest.write(self.ndvi.astype(rasterio.float64))
-
 
     def _evi_square(self):
         """parses the b4, b8, and b2 into an EVI array around the farm"""
@@ -100,9 +103,9 @@ class SentinelPass:
         self.evi = 2.5 * (self.nir - self.red) / ((self.nir + 6.0 * self.red - 7.5 * self.blue) + 1.0)
         b4_out_meta.update({'dtype': rasterio.float64})
 
-        with rasterio.open('evi_masked.tiff', 'w', **b4_out_meta) as dest: # refactor to include datetime info
+        self.evi_gtf_name = f"./pics/evi_{self.sense_date.strftime('%Y%m%d')}.tiff"
+        with rasterio.open(self.evi_gtf_name, 'w', **b4_out_meta) as dest:
             dest.write(self.evi.astype(rasterio.float64))
-
 
     def _paddock_mask(self, paddock_name: str):
         """Accepts paddock name and returns the UTM converted mask, epsg:32616"""
@@ -111,45 +114,88 @@ class SentinelPass:
         paddock_geom = all_geom[all_geom.name == paddock_name]
         return paddock_geom.to_crs('epsg:32616')
 
+    def _name_to_time(self, b2_name: str):
+        split_name = b2_name.split('_')
+        self.sense_date = dt.strptime(split_name[1], '%Y%m%dT%H%M%S')
 
     def paddock_stats(self, paddock_name: str, index: str = 'ndvi') -> dict:
         """accepts geodataframe for specific paddock and index ('ndvi' or 'evi') and computes statistics"""
         paddock_mask = self._paddock_mask(paddock_name)  # pull data from geojson and convert
 
         if index.lower() == 'evi':
-            index_path = './evi_masked.tiff'
+            index_path = './pics/evi_masked.tiff'
         elif index.lower() == 'ndvi':
-            index_path = './ndvi_masked.tiff'
+            index_path = './pics/ndvi_masked.tiff'
         else:
             print('Invalid index chosen. Chose either "evi" or "ndvi"')
             return {'result': 'Invalid index parameter provided'}
 
-
         with rasterio.open(index_path, 'r') as src:
             paddock_data, _ = rasterio.mask.mask(src, paddock_mask.geometry, crop=True)
             pdk_data_dict = {'index': index,
-                paddock_name: {
-                'max': np.nanmax(paddock_data),
-                'min': np.nanmin(paddock_data),
-                'mean': np.nanmean(paddock_data),
-                'median': np.nanmedian(paddock_data),
-                'std': np.nanstd(paddock_data),
-                'var': np.nanvar(paddock_data),
-                'pixels': np.count_nonzero(paddock_data)
-            }}
+                             paddock_name: {
+                                 'max': np.nanmax(paddock_data),
+                                 'min': np.nanmin(paddock_data),
+                                 'mean': np.nanmean(paddock_data),
+                                 'median': np.nanmedian(paddock_data),
+                                 'std': np.nanstd(paddock_data),
+                                 'var': np.nanvar(paddock_data),
+                                 'pixels': np.count_nonzero(paddock_data)
+                             }}
         return pdk_data_dict
-
 
     def available_paddocks(self):
         """Processes the GeoJSON path in use by the instance and returns a list of names"""
-        return list(self.all_mask['name'])
+        return list(self._all_mask['name'])
+
+    def geotiff_to_png(self, index: str):
+        """This method will accept the index (ndvi/evi) and process the geotiff image to produce a masked and transparent
+        image in png format."""
+        if index.lower() == 'ndvi':
+            with rasterio.open(self.ndvi_gtf_name, 'r') as src:
+                img_arr = src.read(1)
+        elif index.lower() == 'evi':
+            with rasterio.open(self.evi_gtf_name, 'r') as src:
+                img_arr = src.read(1)
+        else:
+            return "You have not selected a valid index."
+
+        img_arr_prep = np.nan_to_num(img_arr, nan=-1.0)
+        img_norm = (img_arr_prep + 1.0) / 2.0  # normalizes array from range -1,1 to range 0,1
 
 
-    def name_to_time(self, b2_name):
-        split_name = b2_name.split('-')
-        self.sense_date = dt.strftime(split_name[2], '%Y%m%dT%H%M%S')
+        # custom colordict for transparent values of a segmented 3 color line
+        cdict = {'red':     ((0.0, 0.65, 0.65),
+                            (0.25, 0.97, 0.97),
+                            (0.5, 1.0, 1.0),
+                            (0.75, 0.53, 0.53),
+                            (1.0, 0.0, 0.0)),
+                 'green':   ((0.0, 0.0, 0.0),
+                            (0.25, 0.55, 0.55),
+                            (0.5, 1.0, 1.0),
+                            (0.75, 0.80, 0.80),
+                            (1.0, 0.41, 0.41)),
+                 'blue':    ((0.0, 0.15, 0.15),
+                            (0.25, 0.32, 0.32),
+                            (0.5, 0.75, 0.75),
+                            (0.75, 0.4, 0.4),
+                            (1.0, 0.22, 0.22)),
+                 'alpha':   ((0.0, 0.0, 0.0),
+                            (0.02, 0.0, 1.0),
+                            (0.03, 1.0, 1.0),
+                            (1.0, 1.0, 1.0))}
+        mymap = colors.LinearSegmentedColormap('mymap', cdict)
 
-    # TODO: use matlab cmap to color the square NDVI/EVI
+        # prepping template to save png with transparency and no border info
+        fig = plt.figure()
+        ax = plt.Axes(fig, [0., 0., 1., 1.])
+        ax.set_axis_off()
+        ax.margins(0)
+        fig.add_axes(ax)
+
+        ax.imshow(img_norm, cmap=mymap)
+        plt.savefig(f"./pics/{index.lower()}_{self.sense_date.strftime('%Y%m%d')}.png", transparent=True)
+        plt.close()
 
     # TODO: connect to database
 
--- project_files/test_runs.py
+++ project_files/test_runs.py
@@ -3,8 +3,7 @@ import numpy as np
 from pprint import pprint as pp
 import os
 
-first_test = SentinelPass('../data/S2A_MSIL2A_20200418T164901_N0214_R026_T16TCM_20200418T210937.SAFE',
-                          './farm_simple.geojson')
-
 if __name__ == '__main__':
-    print(first_test.paddock_stats('Paddock 14'))
+    first_test = SentinelPass('S2A_MSIL2A_20200418T164901_N0214_R026_T16TCM_20200418T210937.SAFE',
+                              './farm_simple.geojson')
+    first_test.geotiff_to_png('ndvi')
