870fa51fdea4cc41158e1c6d787740290307bef2
Mark Nyberg <bigusdeveloper@gmail.com>
2020-05-05 17:38:43 -0400

    Compiled stats for cloud determination processing.

---
+++ project_files/index_analysis.py
@@ -0,0 +1,19 @@
+import json
+
+with open('./stats/20200418_Farm boundary_evi.txt', 'r') as infile:
+    clear_sky = json.load(infile)
+
+with open('./stats/20200420_Farm boundary_evi.txt', 'r') as infile:
+    few_clouds = json.load(infile)
+
+with open('./stats/20200425_Farm boundary_evi.txt', 'r') as infile:
+    total_clouds = json.load(infile)
+
+with open('evi_comparison.txt', 'w') as format_data:
+    format_data.write(f"{'Name':>10}{'Clear':^15}{'Partly cloudy':^15}{'Overcast':^15}\n")
+
+    for k, _ in clear_sky['Farm boundary'].items():
+        format_data.write(f"{k:>10}"
+              f"{clear_sky['Farm boundary'][k]:^15.3f}"
+              f"{few_clouds['Farm boundary'][k]:^15.3f}"
+              f"{total_clouds['Farm boundary'][k]:^15.3f}\n")
